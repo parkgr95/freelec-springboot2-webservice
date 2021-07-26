5f6b768ca96be180f98b7ee40044a91c3ba55338
82106 <pkr951113@gmail.com>
2021-07-27 03:35:07 +0900

    0727

---
+++ src/main/java/com/parkgr95/springboot/domain/posts/Posts.java
@@ -0,0 +1,41 @@
+package com.parkgr95.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor // 기본 생성자 자동 추가
+// public Posts() {}와 같은 효과
+@Entity // 테이블과 링크될 클래스
+// Entity 클래스에서는 절대 Setter 메소드를 만들지 않는다.
+public class Posts {
+
+    @Id // 해당 테이블의 PK필드
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // GeneratedValue: PK의 생성 규칙
+    private Long id;
+
+    @Column(length = 500, nullable = false) // Column: 테이블의 칼럼을 나타내며 굳이 선언하지 않더라도 해당 클래스의 필드는 모두 칼럼
+    // 기본값 외에 추가로 변경이 필요한 옵션이 있으면 사용
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+}
---
+++ src/main/java/com/parkgr95/springboot/web/PostApiController.java
@@ -0,0 +1,30 @@
+package com.parkgr95.springboot.web;
+
+import com.parkgr95.springboot.service.posts.PostService;
+import com.parkgr95.springboot.web.dto.PostsResponseDto;
+import com.parkgr95.springboot.web.dto.PostsSaveRequestDto;
+import com.parkgr95.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostApiController {
+
+    private final PostService postsService;
+
+    @PostMapping("/api/v1/posts") //등록
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}") //수정
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}") //조회
+    public PostsResponseDto findbyID (@PathVariable Long id) {
+        return postsService.finById(id);
+    }
+}
