48eb147919e2eba4ac7d6a06b690dacee1bf7a5d
parkgr95 <pkr951113@gmail.com>
2022-01-05 09:22:31 +0900

    0105

--- src/main/java/com/parkgr95/springboot/Application.java
+++ src/main/java/com/parkgr95/springboot/Application.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing // JPA Auditing 활성화
-@SpringBootApplication
+@SpringBootApplication // 스프링 부트의 자동 설정, 스프링 Bean 읽기와 생성을 모두 자동으로 설정
+// 프로젝트의 "메인 클래스"
+// 항상 "프로젝트의 최상단에 위치"
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
--- src/test/java/com/parkgr95/springboot/web/HelloControllerTest.java
+++ src/test/java/com/parkgr95/springboot/web/HelloControllerTest.java
@@ -14,13 +14,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class) // 테스트를 진행할 때 JUnit에 내장된 실행자 외에 다른 실행자(SpringRunner)를 실행시킴.
 // 즉, 스프링 부트 테스트와 JUnit 사이에 연결자 역할을 함.
-@WebMvcTest(controllers = HelloController.class) // Web(Spring MVC)에 집중할 수 있는 어노테이션.
+@WebMvcTest(controllers = HelloController.class) // Web(Spring MVC)에 집중할 수 있는 *어노테이션.
 // 선언할 경우 @Controller, @ControllerAdvice 등을 사용할 수 있음.
 // 단, @Service, @Component, @Repository 등은 사용할 수 없음.
-// 어노테이션: 실행시(런타임시) 특정 기능을 실행하도록 정보를 제공
+// *어노테이션: 실행시(런타임시) 특정 기능을 실행하도록 정보를 제공
 public class HelloControllerTest {
 
-    @Autowired // 스프링이 관리하는 빈(Bean)을 주입.
+    @Autowired // 스프링이 관리하는 빈(Bean)을 주입 받는다.
     private MockMvc mvc; // 웹 API를 테스트할 때 사용
     // 스프링 MVC 테스트의 시작점
     // 이 클래스를 통해서 HTTP GET, POST 등에 대한 API 테스트를 할 수 있음.
